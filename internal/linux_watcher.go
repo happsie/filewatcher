7e22911c13c46ec5ad09eaba12fe440d0e579db9
happsie <njesper98@gmail.com>
2025-02-20 11:24:34 +0100

    start working on a full version using inotify and sys

---
+++ internal/linux_watcher.go
@@ -0,0 +1,99 @@
+package internal
+
+import (
+	"fmt"
+	"io/fs"
+	"log/slog"
+	"os"
+	"path/filepath"
+	"time"
+	"unsafe"
+
+	"golang.org/x/sys/unix"
+)
+
+type ModificationType int32
+
+const (
+	Modified ModificationType = 0
+	Deleted  ModificationType = 1
+	Created  ModificationType = 2
+)
+
+type WatchEvent struct {
+	EventType ModificationType
+	FileName  string
+}
+
+type linuxFileWatcher struct {
+	dir           string
+	watchWithPath map[int]string
+}
+
+func NewLinuxFileWatcher(dir string) *linuxFileWatcher {
+	return &linuxFileWatcher{
+		dir:           dir,
+		watchWithPath: make(map[int]string),
+	}
+}
+
+func (lw *linuxFileWatcher) Watch(eventChan chan WatchEvent) error {
+	if eventChan == nil {
+		return fmt.Errorf("an event channel cannot be nil")
+	}
+	fd, err := unix.InotifyInit()
+	if err != nil {
+		return err
+	}
+	defer unix.Close(fd)
+
+	files, err := fs.ReadDir(os.DirFS(lw.dir), ".")
+	if err != nil {
+		return err
+	}
+	for _, file := range files {
+		watchDescriptor, err := unix.InotifyAddWatch(fd, filepath.Join(lw.dir, file.Name()), unix.IN_CREATE|unix.IN_MODIFY|unix.IN_DELETE)
+		if err != nil {
+			return err
+		}
+		lw.watchWithPath[watchDescriptor] = file.Name()
+	}
+	buf := make([]byte, unix.SizeofInotifyEvent*len(files)*500)
+	slog.Info("watching", "dir", lw.dir)
+	for {
+		n, err := unix.Read(fd, buf)
+		if err != nil {
+			return err
+		}
+		for offset := 0; offset < n; {
+			fmt.Println("offset", offset, "n", n)
+			event := (*unix.InotifyEvent)(unsafe.Pointer(&buf[offset]))
+			fmt.Println("inotify", unix.SizeofInotifyEvent, "event", event)
+			modType, err := getModificationType(event.Mask)
+			if err != nil {
+				slog.Error("error transforming watch to event", "error", err)
+				continue
+			}
+			watchEvent := WatchEvent{
+				EventType: modType,
+				FileName:  lw.watchWithPath[int(event.Wd)],
+			}
+			eventChan <- watchEvent
+			offset += unix.SizeofInotifyEvent + int(event.Len)
+			time.Sleep(500 * time.Millisecond)
+		}
+	}
+}
+
+func getModificationType(mask uint32) (ModificationType, error) {
+	if mask&unix.IN_DELETE == unix.IN_DELETE {
+		return Deleted, nil
+	}
+	if mask&unix.IN_CREATE == unix.IN_CREATE {
+		return Created, nil
+	}
+	if mask&unix.IN_MODIFY == unix.IN_MODIFY {
+		return Modified, nil
+	}
+	return -1, fmt.Errorf("unknown modification type")
+}
--- main.go
+++ main.go
@@ -2,10 +2,28 @@ package main
 
 import (
 	"log/slog"
-	"os"
+
+	"github.com/happsie/filey/internal"
 )
 
 func main() {
+	eventChan := make(chan internal.WatchEvent)
+	watcher := internal.NewLinuxFileWatcher("./test")
+	go func() {
+		err := watcher.Watch(eventChan)
+		if err != nil {
+			slog.Error("error watching directory", "error", err)
+		}
+	}()
+
+	for {
+		select {
+		case <-eventChan: 
+			slog.Info("new event", "event", <-eventChan)
+	}
+
+	}
+		/*
 	watcher := NewWatcher(NewCachedDirectoryScanner())
 
 	watcher.HandleFunc(Created, func(meta FileMeta) {
@@ -14,10 +32,13 @@ func main() {
 	watcher.HandleFunc(Modified, func(meta FileMeta) {
 		slog.Info("handling modified", "meta", meta)
 	})
+	watcher.HandleFunc(Removed, func(meta FileMeta) {
+		slog.Info("handling removed", "meta", meta)
+	})
 
 	err := watcher.Watch()
 	if err != nil {
 		slog.Error("watcher failed", "error", err)
 		os.Exit(1)
-	}
+	}*/
 }
